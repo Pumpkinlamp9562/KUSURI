3f26ad5aa9fb06592afe8d6edb1241e2bc97bf10
Pumpkinlamp9562 <61682407+Pumpkinlamp9562@users.noreply.github.com>
2022-02-24 01:22:43 +0800

    2022/02/24 - Change : Start_UI
    完成開頭的劇情擺放、漸變、翻頁，還有與第一關的銜接

--- KUSURI_0218_2020.3.13/Assets/Scripts/UI/Effects/PagesFlip.cs
+++ KUSURI_0218_2020.3.13/Assets/Scripts/UI/Effects/PagesFlip.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 
 public class PagesFlip : MonoBehaviour
 {
+    [SerializeField] bool dontFlip;
     [SerializeField] GameObject nextPage;
     [SerializeField] float smooth = 0.01f;
+    [SerializeField] float fadeSmooth = 0.1f;
     Button button;
     Material flipShader;
     GameManager manager;
@@ -14,6 +16,8 @@ public class PagesFlip : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (dontFlip)
+            gameObject.AddComponent<UIFade>();
         if (nextPage != null)
             nextPage.GetComponent<Button>().interactable = false;
         manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
@@ -26,19 +30,35 @@ public class PagesFlip : MonoBehaviour
 
     void ButtonOnClick()
     {
+        if(nextPage == null)
+        {
+            manager.scenes.ChangeScene("LV1");
+            return;
+        }
+
+        if (dontFlip)
+        {
+            button.interactable = false;
+            nextPage.GetComponent<Button>().interactable = true;
+            manager.uiNav.UISelectedUpdate(nextPage);
+            gameObject.GetComponent<UIFade>().fadeSpeed = fadeSmooth;
+            gameObject.GetComponent<UIFade>().FadeInOut(0);
+            return;
+        }
         StartCoroutine(Lerp());
     }
 
     IEnumerator Lerp()
     {
-        while(Mathf.Abs(flipShader.GetFloat("_Flip") - -0.5f) > smooth)
+        while (Mathf.Abs(flipShader.GetFloat("_Flip") - -0.5f) > smooth)
         {
-            if(Mathf.Abs(flipShader.GetFloat("_Flip") - -0.5f) < smooth + 0.01f && nextPage != null)
+            if(Mathf.Abs(flipShader.GetFloat("_Flip") - -0.5f) < (smooth + 0.01f))
             {
+                manager.uiNav.UISelectedUpdate(nextPage);
                 button.interactable = false;
                 gameObject.GetComponent<Image>().enabled = false;
                 nextPage.GetComponent<Button>().interactable = true;
-                manager.uiNav.UISelectedUpdate(nextPage);
+                StopAllCoroutines();
             }
             flipShader.SetFloat("_Flip", Mathf.Lerp(flipShader.GetFloat("_Flip"), -0.5f, smooth));
             yield return null;
